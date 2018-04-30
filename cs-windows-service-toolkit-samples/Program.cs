1c17526e5398eea5bc44a884e49d7d4dc78643ad
xschen <cschen1205@gmail.com>
2018-04-30 12:18:38 +0800

    source upload

---
+++ cs-windows-service-toolkit-samples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceToolkit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serviceName = "notepad";
+            int timeoutMilliseconds = 1000;
+            WinServiceUtil.StartService(serviceName, timeoutMilliseconds);
+            WinServiceUtil.IsServiceInstalled(serviceName);
+            WinServiceUtil.StopService(serviceName, timeoutMilliseconds);
+            WinServiceUtil.RestartService(serviceName, timeoutMilliseconds);
+        }
+    }
+}
